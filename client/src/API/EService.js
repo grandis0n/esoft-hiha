ece95960b46a76380d68883302ad23794bd12fad
Ilya Bizimov <ilya.bizimov@mail.ru>
2023-12-06 20:31:26 +1000

    upd

--- client/src/API/EService.js
+++ client/src/API/EService.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default class EService {
     static async getClients(props){
-        const response = await fetch("https://esoft.onrender.com/api/agent/all", props)
+        const response = await fetch("https://esoft-hiha.onrender.com/api/agent/all", props)
         return response.data
     }
 }
